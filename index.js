b0a78fc32d9f0492961c6b6cb12e034655e453aa
Konner Hartman <konner.hartman@yahoo.com>
2021-10-15 16:48:14 -0500

    added code to index.js. Getting errors about conductors.

---
+++ index.js
@@ -0,0 +1,159 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = ('./src/generateHTML.js')
+const Manager = ('./lib/Manager.js');
+const Engineer = ('./lib/Engineer.js');
+const Intern = ('./lib/Intern.js');
+let employeeArray = [];
+
+const role =  [ 
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is this employees role?',
+        choices: ['Engineer', 'Intern', 'Exit']
+    }
+];
+
+const employee = {
+Manager: [            
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of this manager?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this manager?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of this manager?',
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is this managers office number?',
+    }
+],
+
+Engineer: [            
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of this engineer?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this engineer?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of this engineer?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is this engineers Github username??',
+    },
+    {
+        type: 'list',
+        name: 'addnew',
+        message: 'Would you like to add another member?',
+        choices: ['Yes', 'No']
+    }
+],
+
+Intern: [            
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of this intern?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of this intern?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of this intern?',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school does this intern attend?',
+    },
+    {
+        type: 'list',
+        name: 'addnew',
+        message: 'Would you like to add another member?',
+        choices: ['Yes', 'No']
+    }
+]};
+    
+function addManager () {
+    inquirer.prompt(employee.Manager)
+    .then(answer => {
+        const newManager = new Manager
+            (
+                answer.name,
+                answer.id,
+                answer.email,
+                answer.office
+            );
+        employeeArray.push(newManager);
+        addNewMember();
+    })
+};
+
+function addNewMember () {
+    inquirer.prompt(role)
+    .then(answer => {
+        if (answer.role === 'Engineer') {
+            inquirer.prompt(employee.Engineer)
+            .then(answer => {
+                const engineer = new Engineer
+                    (
+                        answer.name,
+                        answer.id,
+                        answer.email,
+                        answer.github
+                    )
+                employeeArray.push(engineer);
+                if (answer.addnew === 'Yes') {
+                    addNewMember();
+                } else {
+                    writeHTML();
+                }
+            })
+        } else if (answer.role === 'Intern') {
+            inquirer.prompt(employee.Intern)
+            .then(answer => {
+                const newIntern = new Intern
+                    (
+                        answer.name,
+                        answer.id,
+                        answer.email,
+                        answer.school
+                    )
+                employeeArray.push(newIntern);
+                if (answer.addnew === 'Yes') {
+                    addNewMember();
+                } else {
+                    writeHTML();
+                }
+            })
+        } else {
+            writeHTML();
+        }
+    })
+};
+addManager();
+
+
